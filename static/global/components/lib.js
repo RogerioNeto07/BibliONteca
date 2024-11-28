6a07f2255837ebd29f34dcd51e213e3543a76099
RogerioNeto07 <joserogeriofonsecauzl@gmail.com>
2024-11-28 11:41:11 -0300

    added icons to sidebar options and made changes to profile screen

--- static/global/components/lib.js
+++ static/global/components/lib.js
@@ -1,4 +1,6 @@
 //Biblioteca para fazer apenas uma única exportação para as tags de bibli, user e guest no arquivo base.html
+import { SidebarGuest } from "./sidebar/guest/index.js";
 import { SidebarUser } from "./sidebar/user/index.js";
 
 customElements.define('sidebar-user', SidebarUser);
+customElements.define('sidebar-guest', SidebarGuest);
--- static/global/components/sidebar/user/index.js
+++ static/global/components/sidebar/user/index.js
@@ -14,7 +14,7 @@ export class SidebarUser extends HTMLElement {
                 </div>
                 <div class="sidebar-item">
                     <div class="sidebar-item-icon">
-                        <img src="static/global/imgs/icon-home.png" alt="" class="sidebar-icon">
+                        <img src="<i class="fa-solid fa-book" style="color: #ffffff;"></i>" alt="" class="sidebar-icon">
                     </div>
                     <div class="sidebar-item-link">
                         <a href="" class="sidebar-link main-text white bold">Empréstimos</a>
@@ -22,7 +22,7 @@ export class SidebarUser extends HTMLElement {
                 </div>
                 <div class="sidebar-item">
                     <div class="sidebar-item-icon">
-                        <img src="static/global/imgs/icon-home.png" alt="" class="sidebar-icon">
+                        <img src="<i class="fa-solid fa-arrow-right-from-bracket" style="color: #ffffff;"></i>" alt="" class="sidebar-icon">
                     </div>
                     <div class="sidebar-item-link">
                         <a href="" class="sidebar-link main-text white bold">Sair</a>
