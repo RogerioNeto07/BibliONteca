a466be88d71040679caf64c9f928724009b7e370
Augusto Rocha <augustochimba12@gmail.com>
2024-11-29 10:37:12 -0300

    feat: add book detail page

---
+++ books/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from books import views
+
+urlpatterns = [
+    path('detalhe', views.ViewDetailBook, name="detail-books"),
+    ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def ViewDetailBook(request):
+    return render(request, "books/detail.html")
