4049bddf1a1204d1c6e1c9cb304891cb60fabfc0
Augusto Rocha <augustochimba12@gmail.com>
2025-01-05 10:39:36 -0300

    feat: create home screen with book cards and carousel

---
+++ user/static/js/carousel.js
@@ -0,0 +1,41 @@
+document.querySelectorAll('.book-section').forEach((section) => {
+    const prevBtn = section.querySelector('.prev-btn');
+    const nextBtn = section.querySelector('.next-btn');
+    const carouselTrack = section.querySelector('.carousel-track');
+    let currentIndex = 0;
+
+    function moveCarousel() {
+        const bookCards = section.querySelectorAll('.book-card');
+        const cardWidth = bookCards[0].offsetWidth + 16;
+        const totalCards = bookCards.length;
+
+        carouselTrack.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+
+        prevBtn.style.display = totalCards > 1 ? 'block' : 'none';
+        nextBtn.style.display = totalCards > 1 ? 'block' : 'none';
+    }
+
+    moveCarousel();
+
+    prevBtn.addEventListener('click', () => {
+        const totalCards = section.querySelectorAll('.book-card').length;
+
+        if (currentIndex > 0) {
+            currentIndex-=3;
+        } else {
+            currentIndex = totalCards - 1;
+        }
+        moveCarousel();
+    });
+
+    nextBtn.addEventListener('click', () => {
+        const totalCards = section.querySelectorAll('.book-card').length;
+
+        if (currentIndex < totalCards - 4) {
+            currentIndex+=3;
+        } else {
+            currentIndex = 0;
+        }
+        moveCarousel();
+    });
+});
