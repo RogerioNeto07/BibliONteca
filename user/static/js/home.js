b2dd059819706390d66d5b5bdf9ca4986855a514
Augusto Rocha <augustochimba12@gmail.com>
2025-01-08 10:14:32 -0300

    feat: add filter for book search

--- user/static/js/home.js
+++ user/static/js/home.js
@@ -1,3 +1,4 @@
+//Carrosel
 document.querySelectorAll('.book-section').forEach((section) => {
     const prevBtn = section.querySelector('.prev-btn');
     const nextBtn = section.querySelector('.next-btn');
@@ -21,7 +22,7 @@ document.querySelectorAll('.book-section').forEach((section) => {
         const totalCards = section.querySelectorAll('.book-card').length;
 
         if (currentIndex > 0) {
-            currentIndex-=3;
+            currentIndex -= 3;
         } else {
             currentIndex = totalCards - 1;
         }
@@ -32,10 +33,43 @@ document.querySelectorAll('.book-section').forEach((section) => {
         const totalCards = section.querySelectorAll('.book-card').length;
 
         if (currentIndex < totalCards - 4) {
-            currentIndex+=3;
+            currentIndex += 3;
         } else {
             currentIndex = 0;
         }
         moveCarousel();
     });
+});
+
+//Filtro
+const openFilterBtn = document.getElementById('openFilterBtn');
+const closeFilterBtn = document.getElementById('closeFilterBtn');
+const filterOverlay = document.getElementById('filterOverlay');
+
+openFilterBtn.addEventListener('click', () => {
+    filterOverlay.classList.add('show'); // Mostra o filtro
+});
+
+closeFilterBtn.addEventListener('click', () => {
+    filterOverlay.classList.remove('show'); // Esconde o filtro
+});
+
+//SELECT
+const genreSelect = document.getElementById("genre");
+const availabilitySelect = document.getElementById("availability");
+
+genreSelect.addEventListener("change", function () {
+    if (genreSelect.value === "") {
+        genreSelect.style.color = "#999999";
+    } else {
+        genreSelect.style.color = "black";
+    }
+});
+
+availabilitySelect.addEventListener("change", function () {
+    if (availabilitySelect.value === "") {
+        availabilitySelect.style.color = "#999999";
+    } else {
+        availabilitySelect.style.color = "black";
+    }
 });
