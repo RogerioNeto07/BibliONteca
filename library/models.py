e9d6f1b70ce8844c13ad2ade03000cc0365a1816
RogerioNeto07 <joserogeriofonsecauzl@gmail.com>
2025-01-17 20:51:46 -0300

    added models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import Leitor
+from books.models import Livro
 
-# Create your models here.
+class Emprestimo(models.Model):
+    leitor = models.ForeignKey(Leitor, on_delete=models.CASCADE, related_name="emprestimos")
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE, related_name="emprestimos")
+    data_emprestimo = models.DateField(null=False, blank=False)
+    previsao_devolucao = models.DateField(null=False, blank=False)
+    data_devolucao = models.DateField(null=True, blank=True)
+    status_ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.leitor.nome} - {self.livro.titulo}"
--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Leitor(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    cpf = models.CharField(max_length=14, unique=True, null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    data_nascimento = models.DateField(null=False, blank=False)
+    telefone = models.CharField(max_length=15, null=False, blank=False)
+    bairro = models.CharField(max_length=100, null=False, blank=False)
+    rua = models.CharField(max_length=100, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
