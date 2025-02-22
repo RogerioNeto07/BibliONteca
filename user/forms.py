3162f5fb5e544e10bc9e257a0518aff71375f185
RogerioNeto07 <joserogeriofonsecauzl@gmail.com>
2025-02-22 19:06:32 -0300

    fix: adjusted home screen and user registration form

--- user/forms.py
+++ user/forms.py
@@ -14,14 +14,14 @@ class UserRegisterForm(UserCreationForm):
             model = MyUser
             fields = ['email', 'nome', 'cpf', 'data_nascimento', 'telefone', 'bairro', 'rua', 'numero', 'foto_perfil', 'password1', 'password2']
             widgets = {
-                'email': forms.EmailInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título'}),
-                'nome': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título'}),
-                'cpf': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título'}),
+                'email': forms.EmailInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'E-mail'}),
+                'nome': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Nome'}),
+                'cpf': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': '999.888.777-66'}),
                 'data_nascimento': forms.DateInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título', 'type': 'date'}),
-                'telefone': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título'}),
-                'bairro': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título'}),
-                'rua': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título'}),
-                'numero': forms.NumberInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título'}),
+                'telefone': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': '(99) 99999-9999'}),
+                'bairro': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Bairro'}),
+                'rua': forms.TextInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Rua'}),
+                'numero': forms.NumberInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Numero'}),
                 'foto_perfil': forms.FileInput(attrs={'class': 'input-form secundary-text bold', 'placeholder': 'Título'}),
             }
 
